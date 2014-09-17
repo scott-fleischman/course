02276dfaf497b85c46f0e22527e3fbaae5943d8f
Scott Fleischman <scott.fleischman@logos.com>
2014-09-16 23:26:25 -0700

    Implement part of Apply

--- src/Course/Apply.hs
+++ src/Course/Apply.hs
@@ -29,8 +29,7 @@ instance Apply Id where
     Id (a -> b)
     -> Id a
     -> Id b
-  (<*>) =
-    error "todo"
+  (<*>) f = flip bindId f . flip mapId
 
 -- | Implement @Apply@ instance for @List@.
 --
@@ -41,8 +40,7 @@ instance Apply List where
     List (a -> b)
     -> List a
     -> List b
-  (<*>) =
-    error "todo"
+  (<*>) f = flip flatMap f . flip map
 
 -- | Implement @Apply@ instance for @Optional@.
 --
@@ -59,8 +57,7 @@ instance Apply Optional where
     Optional (a -> b)
     -> Optional a
     -> Optional b
-  (<*>) =
-    error "todo"
+  (<*>) f = flip bindOptional f . flip mapOptional
 
 -- | Implement @Apply@ instance for reader.
 --
@@ -111,8 +108,7 @@ lift2 ::
   -> f a
   -> f b
   -> f c
-lift2 =
-  error "todo"
+lift2 f a b = f <$> a <*> b
 
 -- | Apply a ternary function in the environment.
 --
@@ -143,8 +139,7 @@ lift3 ::
   -> f b
   -> f c
   -> f d
-lift3 =
-  error "todo"
+lift3 f a b c = f <$> a <*> b <*> c
 
 -- | Apply a quaternary function in the environment.
 --
@@ -176,8 +171,7 @@ lift4 ::
   -> f c
   -> f d
   -> f e
-lift4 =
-  error "todo"
+lift4 f a b c d = f <$> a <*> b <*> c <*> d
 
 -- | Sequence, discarding the value of the first argument.
 -- Pronounced, right apply.
